657ae625b210e55f0e096a7bb43809b291becdcd
Matthew-Molloy <mmolloy@nevada.unr.edu>
2016-10-28 19:18:35 -0700

    Assets

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace AssemblyCSharp
+{
+	public class test : MonoBehaviour {
+
+		Vehicle v;
+
+		// Use this for initialization
+		void Start () {
+			v = new Vehicle();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
