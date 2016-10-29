554a62f9bf2e1fcb0575d31c5efba9e72a1c8b56
Matthew-Molloy <mmolloy@nevada.unr.edu>
2016-10-28 19:30:27 -0700

    Class files

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
-namespace AssemblyCSharp
-{
-	public class test : MonoBehaviour {
+using AssemblyCSharp;
 
-		Vehicle v;
 
-		// Use this for initialization
-		void Start () {
-			v = new Vehicle();
-		}
+public class test : MonoBehaviour {
+
+	Vehicle v;
+
+	// Use this for initialization
+	void Start () {
+		v = new Vehicle();
+	}
+	
+	// Update is called once per frame
+	void Update () {
 		
-		// Update is called once per frame
-		void Update () {
-			
-		}
 	}
 }
